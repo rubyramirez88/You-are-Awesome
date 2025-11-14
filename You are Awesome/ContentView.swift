08eb8ffba75fb549f6a7cde0d745304a06320e3e
rubyramirez88 <ruizruby160@gmail.com>
2025-11-14 13:32:01 -0800

    Initial Commit

---
+++ You are Awesome/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  You are Awesome
+//
+//  Created by RAMIREZ, RUBY on 11/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = "I Am A Programmer!"
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            Button("Click Me!") {
+                message = "Awesome!"
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ You are Awesome/You_are_AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  You_are_AwesomeApp.swift
+//  You are Awesome
+//
+//  Created by RAMIREZ, RUBY on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct You_are_AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
